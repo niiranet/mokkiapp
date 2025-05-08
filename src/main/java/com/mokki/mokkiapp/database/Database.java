43614b0bb37f58691553875cca16555514d938bc
NappiPastori <oskari.klemola47@gmail.com>
2025-05-08 15:27:01 +0300

    Erilähestyminen tietokannan yhdistämiseen.

--- src/main/java/com/mokki/mokkiapp/database/Database.java
+++ src/main/java/com/mokki/mokkiapp/database/Database.java
@@ -1,68 +1,31 @@
 package com.mokki.mokkiapp.database;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.*;
-import java.util.Properties;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * tämä luokka hoitaa tietokantaan yhdistämisen ja siihen liittyvän virhekäsittelyn
  */
 public class Database {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/lomakyla";
+    private static final String USER = "root"; // Korvaa omalla käyttäjätunnuksellasi tai voi olla automaattisesti root
+    private static final String PASSWORD = ""; // Korvaa omalla salasanallasi
+
     public static void main(String[] args) {
+        System.out.println("Nykyinen työhakemisto: " + System.getProperty("user.dir"));
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                System.out.println("Tietokantayhteys onnistui!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Tietokantayhteyden muodostaminen epäonnistui:");
+            e.printStackTrace();
+        }
     }
 
     public static Connection getConnection() throws SQLException {
-        // tämä on kaikilla sama, kun käytetään setup_test.sql
-        String URL = "jdbc:mysql://localhost:3306/lomakyla";
-        /*
-        // TODO: saada tämä toimimaan mielummin
-        // nämä haetaan ympäristömuuttujina
-        String user = System.getenv("OTI_USER");
-        String password = System.getenv("OTI_PASS");
-
-        if (user != null && password != null) {
-            System.out.println("Environment variables: OK");
-        }
-        else {
-            System.out.println("Environment variables: null");
-            System.exit(1);
-        }
-        */
-
-        // vaihtoehtoinen ratkaisu
-        String USER = null;
-        String PASSWORD = null;
-        Properties properties = new Properties();
-        try (FileInputStream fis = new FileInputStream("src/db_credentials.properties")) {
-            properties.load(fis);
-            USER = properties.getProperty("db.username");
-            PASSWORD = properties.getProperty("db.password");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        // yhteyden luominen mysql serveriin
         return DriverManager.getConnection(URL, USER, PASSWORD);
-
-
-        /*
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            if (connection != null) {
-                System.out.println("Connected to the src.database.database!" + "\n");
-                Statement statement = connection.createStatement();
-
-                //suoritaTestikyselyt(statement);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        */
-
     }
-
 }
