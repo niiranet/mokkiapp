ec0d9552311821f0c0fa53d12944f173dd996c99
niiranet <tniirane@uef.fi>
2025-05-08 23:01:17 +0300

    Laskutusnäkymän toiminnallisuudet
    -Lisätty aineiston lataus apin käynnistyksen yhteydessä.
    -Lisätty toiminnallisuudet laskujen suodattamiselle.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mokki.mokkiapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.mokki.mokkiapp to javafx.fxml;
