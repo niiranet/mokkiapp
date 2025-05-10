9aa71b268e2c377e67c91061916c21338590e90d
jarkkisk <183512585+jarkkisk@users.noreply.github.com>
2025-05-10 13:29:34 +0300

    Tunnusten hakeminen db_credentials tiedostosta toimii

--- src/main/java/com/mokki/mokkiapp/database/Database.java
+++ src/main/java/com/mokki/mokkiapp/database/Database.java
@@ -1,31 +1,38 @@
 package com.mokki.mokkiapp.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 
 /**
- * tämä luokka hoitaa tietokantaan yhdistämisen ja siihen liittyvän virhekäsittelyn
+ * Hoitaa tietokantaan yhdistämisen ja siihen liittyvän virhekäsittelyn
  */
 public class Database {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/lomakyla";
-    private static final String USER = "root"; // Korvaa omalla käyttäjätunnuksellasi tai voi olla automaattisesti root
-    private static final String PASSWORD = ""; // Korvaa omalla salasanallasi
-
     public static void main(String[] args) {
-        System.out.println("Nykyinen työhakemisto: " + System.getProperty("user.dir"));
-        try (Connection connection = getConnection()) {
-            if (connection != null) {
-                System.out.println("Tietokantayhteys onnistui!");
-            }
-        } catch (SQLException e) {
-            System.err.println("Tietokantayhteyden muodostaminen epäonnistui:");
-            e.printStackTrace();
-        }
     }
 
     public static Connection getConnection() throws SQLException {
+        // tämä on kaikilla sama, kun käytetään setup_test.sql
+        String URL = "jdbc:mysql://localhost:3306/lomakyla";
+
+        String USER = null;
+        String PASSWORD = null;
+        Properties properties = new Properties();
+        //try (FileInputStream fis = new FileInputStream("src/db_credentials.properties")) {
+        try (FileInputStream fis = new FileInputStream("src/main/java/com/mokki/mokkiapp/db_credentials.properties")) {
+            properties.load(fis);
+            USER = properties.getProperty("db.username");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // yhteyden luominen mysql serveriin
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
